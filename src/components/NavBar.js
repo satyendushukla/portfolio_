aff7046e3864cb33308bcbbb6bbf71a6ddc2b0e0
satyendushukla <satyendu.21b1541157@abes.ac.in>
2024-06-02 23:56:50 +0530

    first

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,6 +3,7 @@ import Link from 'next/link'
 import Logo from './Logo'
 import { useRouter } from 'next/router'
 import {TwitterIcon, DribbbleIcon, GithubIcon, LinkedInIcon, PinterestIcon, SunIcon } from './Icons'
+import {motion} from 'framer-motion'
 
 const CustomLink=({href,title,className =""})=>{
   const router =useRouter();
@@ -29,12 +30,34 @@ const NavBar = () => {
             <CustomLink href="/articles"title="Articles"className='ml-4'/>
         </nav>
         
-        <nav>
-            <Link href="/" target={"_blank"}><TwitterIcon/></Link>
-            <Link href="/" target={"_blank"}><GithubIcon/></Link>
-            <Link href="/" target={"_blank"}><LinkedInIcon/></Link>
-            <Link href="/" target={"_blank"}><PinterestIcon/></Link>
-            <Link href="/" target={"_blank"}><DribbbleIcon/></Link>
+        <nav className='flex items-center justify-center flex-wrap'>
+            <motion.a href="/" target={"_blank"}
+            whileHover={{y:-2}}
+            whileTap={{scale:0.9}}
+            className='w-6 mx-3'
+            
+            ><TwitterIcon/>
+            </motion.a>
+            <motion.a href="/" target={"_blank"}
+             whileHover={{y:-2}}
+            whileTap={{scale:0.9}}
+            className='w-6 mx-3'
+            ><GithubIcon/></motion.a>
+            <motion.a href="/" target={"_blank"}
+             whileHover={{y:-2}}
+            whileTap={{scale:0.9}}
+            className='w-6 mx-3'
+            ><LinkedInIcon/></motion.a>
+            <motion.a href="/" target={"_blank"}
+             whileHover={{y:-2}}
+            whileTap={{scale:0.9}}
+            className='w-6 mx-3'
+            ><PinterestIcon/></motion.a>
+            <motion.a href="/" target={"_blank"}
+             whileHover={{y:-2}}
+            whileTap={{scale:0.9}}
+            className='w-6 ml-3'
+            ><DribbbleIcon/></motion.a>
 
         </nav>
         <div className='absolute left-[50%] top-2 translate-x-[-50%]'>
